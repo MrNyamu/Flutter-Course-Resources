8e7ff8950f038d727762e2ba04185e9544080f5f
MrNyamu <wesley@fingo.holdings>
2022-10-05 11:33:41 +0300

    New Flutter App
    New Flutter App

---
+++ business_card_flutter/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 100.0,
+            height: 100.0,
+            color: Colors.white,
+            child: Text('Container 1'),
+          ),
+          Container(
+            width: 100.0,
+            height: 100.0,
+            color: Colors.yellow,
+            child: Text('Container 2'),
+          ),
+          Container(
+            width: 100.0,
+            height: 100.0,
+            color: Colors.red,
+            child: Text('Container 3'),
+          ),
+          Container(
+            width: 100.0,
+            height: 100.0,
+            color: Colors.blue,
+            child: Text('Container 4'),
+          )
+        ],
+      )),
+    ));
+  }
+}
